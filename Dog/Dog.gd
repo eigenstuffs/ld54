8aa5a1b0d1790022ca6b52ff584ed23c054bf4e1
branden <88228182+kiratfup@users.noreply.github.com>
2023-09-30 14:20:11 -0700

    dog collider

--- Dog/Dog.gd
+++ Dog/Dog.gd
@@ -7,10 +7,10 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
 func _physics_process(delta):
-#	if not is_on_floor():
-#		velocity.y -= gravity * delta
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+#	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+#		velocity.y = JUMP_VELOCITY
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
