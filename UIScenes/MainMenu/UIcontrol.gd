eaadb002c17d3972daa8121d0fc3706d210fb5cc
Clea <clea_h@p-hall.net>
2023-09-30 22:25:43 -0700

    UI Update

--- UIScenes/MainMenu/UIcontrol.gd
+++ UIScenes/MainMenu/UIcontrol.gd
@@ -1,24 +1,16 @@
 extends Control
 
-
-
 func _on_play_button_pressed():
-	get_tree().change_scene_to_file("res://Dog/test.tscn")
-
+	get_tree().change_scene_to_file("res://Levels/lvl1/lvl1.tscn")
 
 func _on_settings_button_pressed():
-	get_tree().change_scene_to_file("res://UIControl/SettingsMenu/settings_menu.tscn")
-
+	get_tree().change_scene_to_file("res://UIScenes/SettingsMenu/settings_menu.tscn")
 
 func _on_credits_button_pressed():
-	get_tree().change_scene_to_file("res://UIControl/Credits/credits_scene.tscn")
-
-
+	get_tree().change_scene_to_file("res://UIScenes/Credits/credits_scene.tscn")
 
 func _on_lvl_select_pressed():
-	get_tree().change_scene_to_file("res://UIControl/LVLSelect/lvl_select.tscn")
-
-
+	get_tree().change_scene_to_file("res://UIScenes/LVLSelect/lvl_select.tscn")
 
 func _on_quit_button_pressed():
 	get_tree().quit()
