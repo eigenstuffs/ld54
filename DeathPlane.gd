dbe6bf012561cd1a68fed472880f625f07199b68
Do3naut <51458829+Do3naut@users.noreply.github.com>
2023-10-01 21:40:33 -0700

    no more bloodshed on bark
    sadge

--- DeathPlane.gd
+++ DeathPlane.gd
@@ -3,5 +3,6 @@ extends Area3D
 func _on_body_entered(body):
 	if body is Sheep:
 		body.queue_free()
+		get_parent().get_node("WorldEnvironment").red_flare()
 	elif body is Dog:
 		get_tree().reload_current_scene()
