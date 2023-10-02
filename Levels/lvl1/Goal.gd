790b824bb00c22edb45b62ca60c607c99d455652
branden <88228182+kiratfup@users.noreply.github.com>
2023-10-02 10:24:36 -0700

    goal fix

--- Levels/lvl1/Goal.gd
+++ Levels/lvl1/Goal.gd
@@ -1,18 +1,25 @@
 extends Area3D
 
 var num_sheep = 0
-var sheepCount = 0
+var sheep_count = 0
 
 func _process(delta):
-	sheepCount = get_tree().get_nodes_in_group("Sheep").size()
-
-func _on_body_entered(body):
-	if body is Sheep:
-		num_sheep += 1
-		if num_sheep >= sheepCount:
-			print("done")
-			LeaderboardBackend.emit_signal("stop_timer")
-
-func _on_body_exited(body):
-	if body is Sheep:
-		num_sheep -= 1
+	sheep_count = get_tree().get_nodes_in_group("Sheep").size()
+	for i in get_overlapping_bodies():
+		if i is Sheep:
+			num_sheep += 1
+	if num_sheep >= sheep_count:
+		print("done")
+		LeaderboardBackend.emit_signal("stop_timer")
+	num_sheep = 0
+#
+#func _on_body_entered(body):
+#	if body is Sheep:
+#		num_sheep += 1
+#		if num_sheep >= sheepCount:
+#			print("done")
+#			LeaderboardBackend.emit_signal("stop_timer")
+#
+#func _on_body_exited(body):
+#	if body is Sheep:
+#		num_sheep -= 1
