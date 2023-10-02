0a0ff1d6d47db0a37236dacbbfa68bc5e68cf7f2
Kenzie MacDougal <kmacd02@g.ucla.edu>
2023-10-01 17:06:20 -0700

    main menu UI assets implemented

---
+++ UIScenes/MainMenu/UIcontrol.gd
@@ -0,0 +1,25 @@
+extends Control
+
+var isShowing = false
+
+func _input(event):
+	if event is InputEventKey && $How2Play.visible:
+		$How2Play.hide()
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://Levels/lvl1/lvl1.tscn")
+
+func _on_settings_button_pressed():
+	get_tree().change_scene_to_file("res://UIScenes/SettingsMenu/settings_menu.tscn")
+
+func _on_credits_button_pressed():
+	get_tree().change_scene_to_file("res://UIScenes/Credits/credits_scene.tscn")
+
+func _on_lvl_select_pressed():
+	get_tree().change_scene_to_file("res://UIScenes/LVLSelect/lvl_select.tscn")
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+
+func _on_how_2_button_pressed():
+	$How2Play.show()
