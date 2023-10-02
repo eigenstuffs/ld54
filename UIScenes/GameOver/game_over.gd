dc42e7f80c71ab3523b03fc46dd479b764437168
Kenzie MacDougal <kmacd02@g.ucla.edu>
2023-10-02 13:24:21 -0700

    add lvl 6 and fix gameover

--- UIScenes/GameOver/game_over.gd
+++ UIScenes/GameOver/game_over.gd
@@ -3,8 +3,7 @@ extends Control
 signal retried
 signal nextlvl
 
-@onready var time : = $CenterContainer/VBoxContainer/Timer
-@onready var title : = $CenterContainer/VBoxContainer/Title
+@onready var time : = $CenterContainer/VBoxContainer/TextureRect/Timer
 
 func initialize(total_play_time : float = 0.0, won : bool = true) -> void:
 #	var minutes : String = str(int(total_play_time / 60.0))
@@ -12,21 +11,19 @@ func initialize(total_play_time : float = 0.0, won : bool = true) -> void:
 #	var time_text = "Total Time : %s m %s s" %[minutes,seconds]
 	show()
 	if won:
-		title.text = "Success!"
 		time.text = "Your time: " + str(snapped(total_play_time, 0.01))
 	else:
 		time.text = "Try again!"
-		title.text = "All your sheep fell off :("
 #	time.text = time_text
 	
-func _on_exit_pressed():
+func _on_quit_pressed():
 	get_tree().change_scene_to_file("res://test/game.tscn")
-	hide()
  
-func _on_nextlvl_pressed():
+func _on_next_pressed():
 	get_tree().current_scene.get_node("Level").change(min(6, get_tree().current_scene.get_node("Level").current_level+1))
 	hide()
 
-func _on_retry_pressed():
+func _on_replay_pressed():
 	get_tree().current_scene.get_node("Level").change(get_tree().current_scene.get_node("Level").current_level)
 	hide()
+
