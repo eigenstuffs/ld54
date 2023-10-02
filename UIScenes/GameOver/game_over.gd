f53fb623e596fecab7e06a67f672764c4c4ba9fd
branden <88228182+kiratfup@users.noreply.github.com>
2023-10-02 13:31:38 -0700

    level 6 fix

--- UIScenes/GameOver/game_over.gd
+++ UIScenes/GameOver/game_over.gd
@@ -10,6 +10,7 @@ func initialize(total_play_time : float = 0.0, won : bool = true) -> void:
 #	var minutes : String = str(int(total_play_time / 60.0))
 #	var seconds : String = str(int(fmod(total_play_time, 60.0)))
 #	var time_text = "Total Time : %s m %s s" %[minutes,seconds]
+	print("showing game over")
 	show()
 	if won:
 		title.text = "Success!"
@@ -24,7 +25,7 @@ func _on_exit_pressed():
 	hide()
  
 func _on_nextlvl_pressed():
-	get_tree().current_scene.get_node("Level").change(min(6, get_tree().current_scene.get_node("Level").current_level+1))
+	get_tree().current_scene.get_node("Level").change(min(6, get_tree().current_scene.get_node("Level").current_level+2))
 	hide()
 
 func _on_retry_pressed():
